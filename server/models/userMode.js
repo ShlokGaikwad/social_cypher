c5e843305dd33e89273040dc0912347bd2af9dd4
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2024-04-11 15:25:01 +0530

    error solver

--- server/models/userMode.js
+++ server/models/userMode.js
@@ -4,10 +4,10 @@ const userSchema = mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true },
-    password:{type:String,required:true},
-    contact:{type:Number,required:true},
+    password: { type: String, required: true },
+    contact: { type: Number, required: true },
     picture: { type: String },
-    friends: { type: [String], default:[]},
+    friends: { type: [String], default: [] },
   },
   {
     versionKey: false,
@@ -16,4 +16,4 @@ const userSchema = mongoose.Schema(
 
 const UserModel = mongoose.model("users", userSchema);
 
-module.exports = {UserModel};
+module.exports = { UserModel };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,60 +1,69 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const {UserModel} = require('../models/userMode');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { UserModel } = require("../models/userMode");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const userRouter = express.Router();
 
-userRouter.post('/signup', async(req, res)=>{
-    try{
-        
-        const {username, email, password, contact} = req.body;
-        const exUser = await UserModel.findOne({email});
-        if(exUser){
-            res.status(409).send({msg: 'user is already registered,Kindly Login'})
-        }
-        
-        bcrypt.hash(pass, 8, async(err, hash)=>{
-            if(hash){
-                const user = new UserModel({username, email, password:hash, contact});
-                await user.save();
-                res.status(201).send({mas: 'new user has benn register', user: username})
-            }else{
-                console.log(err);
-                res.status(404).send({msg:'error in password hashing process',err:err})
-            }
-        })
-        
-
-    }catch(err){
-        console.log(err);
-        res.status(500).send({msg: "Error while registering user", err})
+userRouter.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password, contact } = req.body;
+    const exUser = await UserModel.findOne({ email });
+    if (exUser) {
+      res.status(409).send({ msg: "user is already registered,Kindly Login" });
     }
-})
 
-
-userRouter.post('/login', async(req, res)=>{
-    const {email, password} = req.body;
-    try{
-        const user = await UserModel.findOne({email});
-        if(!user){
-            res.status(404).send({msg: 'User not found please register'});
-        }else{
-            bcrypt.compare(pass,user.password, (err,result) =>{
-                if(result){
-                    const token = jwt.sign({userID:user._id,author:user.username},"Abhay",{expiresIn:'7d'})
-                    res.status(200).send({msg:'login successfully',token})
-                }else{
-                    return res.status(401).send({ error: 'Wrong password.' });
-                }
-            })
-        }
-    }catch(err){
+    bcrypt.hash(password, 8, async (err, hash) => {
+      if (hash) {
+        const user = new UserModel({
+          username,
+          email,
+          password: hash,
+          contact,
+        });
+        await user.save();
+        res
+          .status(201)
+          .send({ mas: "new user has benn register", user: username });
+      } else {
         console.log(err);
-        res.status(404).send({msg:"error in user login",errors:err})
-    }
-})
+        res
+          .status(404)
+          .send({ msg: "error in password hashing process", err: err });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "Error while registering user", err });
+  }
+});
 
-module.exports = {userRouter}
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      res.status(404).send({ msg: "User not found please register" });
+    } else {
+      bcrypt.compare(pass, user.password, (err, result) => {
+        if (result) {
+          const token = jwt.sign(
+            { userID: user._id, author: user.username },
+            "Abhay",
+            { expiresIn: "7d" }
+          );
+          res.status(200).send({ msg: "login successfully", token });
+        } else {
+          return res.status(401).send({ error: "Wrong password." });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404).send({ msg: "error in user login", errors: err });
+  }
+});
+
+module.exports = { userRouter };
